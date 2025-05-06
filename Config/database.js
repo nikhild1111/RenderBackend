7315f328a2191876b31db420a291889657bccc8e
nikhild1111 <160055706+nikhild1111@users.noreply.github.com>
2025-05-06 18:05:25 +0530

    Mongo chnages

--- Config/database.js
+++ Config/database.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 exports.dbConnect=()=>{
     // The `uri` parameter to `openUri()` must be a string, got "undefined"
 // this means the url to the connect is undifind
-console.log(process.env.DATABASE_URL);
-mongoose.connect("mongodb://localhost:27017/Shop_app")
+// console.log(process.env.DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL)
     .then(()=>{
         console.log("Connection With the database is Done");
 
